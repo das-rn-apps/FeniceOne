6822803d2d6310a35f5241d4e615bf3aff48b0e8
Deepak-Das-7 <deepak.d@feniceenergy.com>
2025-11-21 12:51:12 +0530

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <div className="w-full bg-white shadow-sm px-6 py-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">Lead Manager</h1>
 
       <div className="flex items-center gap-4">
-        <a href="/" className="hover:text-blue-600">LeadsV1</a>
-        <a href="/create" className="hover:text-blue-600">Create</a>
+        <Link to="/" className="hover:text-blue-600">LeadsV1</Link>
+        <Link to="/create" className="hover:text-blue-600">Create</Link>
       </div>
     </div>
   );
